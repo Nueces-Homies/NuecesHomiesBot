46013a69a9ead12aee883dd46e98588f6cfac4f1
Zuhair Parvez <zuhairparvez@gmail.com>
2022-12-31 10:16:09 -0800

    Add Crystal ID (#2)
    Crystal is our take on [Discord](https://discord.com/developers/docs/reference#snowflakes) or [Twitter's](https://blog.twitter.com/engineering/en_us/a/2010/announcing-snowflake) Snowflake and [UUID7](https://www.ietf.org/archive/id/draft-peabody-dispatch-new-uuid-format-01.html#section-4.4). Since we do not operate at scale and are not distributed, we forgo encoding machine/worker ID (like in Snowflake) and use only 64-bits (unlike UUID7 which is 128-bit).
    
    The format of the Crystal is as follows:
    
    | Start Bit | End Bit | Length | Description
    --------- |---------|-------|-------------
    64 | 16 | 48 | Milliseconds since Unix epoch
    15 | 0 | 16 | Randomly generated noise
    
    
    This allows for randomly generated, but ordered, IDs that we can use as primary keys in our database.

---
+++ Core.Test/CrystalTest.cs
@@ -0,0 +1,43 @@
+namespace Core.Test;
+
+using FluentAssertions;
+
+public class CrystalTest
+{
+    [Fact]
+    public void RapidGenerationIsMonotonicallyIncreasing()
+    {
+        var crystals = new Crystal[10000];
+        for (var i = 0; i < crystals.Length; i++)
+        {
+            crystals[i] = Crystal.New();
+        }
+
+        var nonRandomBits = crystals.Select(c => c.WithoutNoise()).ToArray();
+        nonRandomBits.Should().BeInAscendingOrder();
+    }
+
+    [Fact]
+    public void SlowGenerationIsStrictlyIncreasing()
+    {
+        var crystals = new Crystal[50];
+        for (var i = 0; i < crystals.Length; i++)
+        {
+            crystals[i] = Crystal.New();
+            Thread.Sleep(20);
+        }
+
+        var nonRandomBits = crystals.Select(c => c.WithoutNoise());
+        nonRandomBits.Should().BeInAscendingOrder().And.OnlyHaveUniqueItems();
+    }
+}
+
+file static class CrystalUtils
+{
+    public static ulong WithoutNoise(this Crystal crystal)
+    {
+        ulong mask = 0xFFFF;
+        mask = ~mask;
+        return crystal.Value & mask;
+    }
+}
---
+++ Core/Crystal.cs
@@ -0,0 +1,26 @@
+namespace Core;
+
+public readonly record struct Crystal (ulong Value)
+{
+    /// <summary>
+    /// Crystal is our take on Discord/Twitter's Snowflake and UUID7. Since we do not operate
+    /// at scale and are not distributed, we forgo encoding machine/worker ID (like in Snowflake)
+    /// and use only 64-bits (unlike UUID7 which is 128-bit).
+    ///
+    /// The format of the Crystal is as follows:
+    /// Bits 64 to 16: Milliseconds since Unix epoch
+    /// Bits 15 to 0: Randomly generated noise
+    ///
+    /// This allows for randomly generated, but ordered, IDs that we can use as primary keys in our database 
+    /// </summary>
+    public static Crystal New()
+    {
+        var milliseconds = (ulong) DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        
+        Span<byte> noiseBytes = stackalloc byte[2];
+        Random.Shared.NextBytes(noiseBytes);
+        var noise = BitConverter.ToUInt16(noiseBytes);
+
+        return new Crystal((milliseconds << 16) + noise);
+    }
+}
