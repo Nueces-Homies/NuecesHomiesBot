f5b0e58f1f5d946d6cf6e6ab64f3f2d608e4aae9
Zuhair Parvez <zuhairparvez@gmail.com>
2023-01-04 23:43:09 -0800

    Add a little more test test coverage

--- HumanTime.Test/HumanTimeTests.cs
+++ HumanTime.Test/HumanTimeTests.cs
@@ -1,5 +1,6 @@
 namespace HumanTime.Test;
 
+using System.Globalization;
 using HumanTimeParser;
 
 public class HumanTimeTests
@@ -8,7 +9,9 @@ public class HumanTimeTests
     {
         DateTimeOffset now = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "America/Chicago");
         DateTimeOffset future = now.AddMonths(2);
-        DateTimeOffset past = new DateTimeOffset(now.Year, 1, 3, 0, 0, 0, TimeSpan.FromHours(-6));
+        
+        // Some tests may fail if run on January 1st or 2nd, so just don't do that :)
+        DateTimeOffset past = new DateTimeOffset(now.Year, 1, 2, 0, 0, 0, TimeSpan.FromHours(-6));
         
         yield return new object[] { $"{future.Month}/{future.Day}", HumanTime.Date(future.Year, future.Month, future.Day) };
         yield return new object[] { $"5/{now.Year + 2}", HumanTime.Month(now.Year + 2, 5) };
@@ -22,7 +25,18 @@ public class HumanTimeTests
         DateTimeOffset yesterday = now.AddDays(-1);
         yield return new object[] { "yesterday", HumanTime.Date(yesterday.Year, yesterday.Month, yesterday.Day) };
 
-        yield return new object[] { "25th of May, 2025", HumanTime.Date(2025, 5, 25) };
+        yield return new object[] { "25th of November, 2025", HumanTime.Date(2025, 11, 25) };
+        yield return new object[] { "25th of October '25", HumanTime.Date(2025, 10, 25) };
+        yield return new object[]
+        {
+            $"{CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(future.Month)} {future.Day.ToOrdinal()}",
+            HumanTime.Date(future.Year, future.Month, future.Day),
+        };
+        yield return new object[]
+        {
+            $"{CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(past.Month)} {past.Day.ToOrdinal()}",
+            HumanTime.Date(past.Year+1, past.Month, past.Day),
+        };
     }
     
     [Theory]
@@ -31,5 +45,27 @@ public class HumanTimeTests
     {
         HumanTime actual = HumanTime.Parse(input);
         Assert.Equal(expected, actual);
+    }
+}
+
+file static class TestExtensions
+{
+    public static string ToOrdinal(this int num)
+    {
+        switch (num % 100)
+        {
+            case 11:
+            case 12:
+            case 13: 
+                return num+"th";
+        }
+
+        return num + (num % 10) switch
+        {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
     }
 }
